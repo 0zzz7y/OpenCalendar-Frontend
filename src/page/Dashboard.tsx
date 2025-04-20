18ef8cf05778e893117e6c66e3547738ca9cd7f7
ozzz7y <0zzz7y@gmail.com>
2025-04-20 21:35:22 +0200

    cos tam dziala

--- src/page/Dashboard.tsx
+++ src/page/Dashboard.tsx
@@ -1,42 +1,19 @@
-import React, { useState } from "react"
-import LeftPanel from "@/component/layout/LeftPanel"
-import CenterPanel from "@/component/layout/CenterPanel"
-import RightPanel from "@/component/layout/RightPanel"
-import CalendarEditor from "@/component/calendar/CalendarEditor"
-import CategoryEditor from "@/component/category/CategoryEditor"
-import useAppContext from "@/hook/context/useAppContext" // Import the custom hook to access AppContext
-import EditorType from "@/type/utility/editorType"
-import CollapsingPanel from "@/component/layout/ResizableLayout"
-import ResizableLayout from "@/component/layout/ResizableLayout"
-import CalendarSelector from "@/component/calendar/CalendarSelector"
-import MonthlyCalendar from "@/component/calendar/MonthlyCalendar"
-import CategorySelector from "@/component/category/CategorySelector"
-import NotesPanel from "@/component/note/NotesPanel"
-import TasksPanel from "@/component/task/TasksPanel"
-import ThemeToggleButton from "@/component/theme/ThemeToggleButton"
-import { Box, Stack } from "@mui/material"
+import React from "react"
+import { Box } from "@mui/material"
 import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels"
 
+import CalendarSelector from "@/component/calendar/CalendarSelector"
+import CalendarEditor from "@/component/calendar/CalendarEditor"
+import CategorySelector from "@/component/category/CategorySelector"
+import CategoryEditor from "@/component/category/CategoryEditor"
+import MonthlyCalendar from "@/component/calendar/MonthlyCalendar"
+import ThemeToggleButton from "@/component/theme/ThemeToggleButton"
+import DraggableNotes from "@/component/note/DraggableNotes"
+import TasksPanel from "@/component/task/TasksPanel"
+import CenterPanel from "../component/layout/CenterPanel"
+import NotesPanel from "@/component/note/NotesPanel"
+
 const Dashboard = () => {
-  // Inicjalizacja szerokości paneli jako stanu
-  const [leftPanelWidth, setLeftPanelWidth] = useState(20); // 20% szerokości ekranu dla lewego panelu
-  const [rightPanelWidth, setRightPanelWidth] = useState(30); // 30% szerokości ekranu dla prawego panelu
-  const [centerPanelWidth, setCenterPanelWidth] = useState(50); // 50% szerokości ekranu dla środkowego panelu
-
-  // Funkcja zmieniająca szerokość lewego panelu
-  const handleLeftPanelResize = (newWidth: number) => {
-    const totalWidth = 100; // Cała szerokość w procentach
-    setLeftPanelWidth(newWidth);
-    setCenterPanelWidth(totalWidth - newWidth - rightPanelWidth); // Aktualizacja szerokości środkowego panelu
-  };
-
-  // Funkcja zmieniająca szerokość prawego panelu
-  const handleRightPanelResize = (newWidth: number) => {
-    const totalWidth = 100; // Cała szerokość w procentach
-    setRightPanelWidth(newWidth);
-    setCenterPanelWidth(totalWidth - leftPanelWidth - newWidth); // Aktualizacja szerokości środkowego panelu
-  };
-
   return (
     <PanelGroup direction="horizontal" style={{ height: "100vh", width: "100%" }}>
       {/* Left Panel */}
@@ -45,8 +22,8 @@ const Dashboard = () => {
           {/* Calendar and Category section */}
           <Panel
             style={{
-              border: "2px solid #ccc", // Obramowanie
-              borderRadius: "8px", // Zaokrąglone rogi
+              border: "2px solid #ccc",
+              borderRadius: "8px",
               padding: "10px",
               margin: "10px 5px",
             }}
@@ -58,12 +35,12 @@ const Dashboard = () => {
               height="100%"
               width="100%"
             >
-              <Stack spacing={2}>
+              <Box display="flex" flexDirection="column" gap={2}>
                 <CalendarSelector />
                 <CalendarEditor />
                 <CategorySelector />
                 <CategoryEditor />
-              </Stack>
+              </Box>
             </Box>
           </Panel>
 
@@ -73,10 +50,11 @@ const Dashboard = () => {
           <Panel
             style={{
               minHeight: "150px",
-              border: "2px solid #ccc", // Obramowanie
-              borderRadius: "8px", // Zaokrąglone rogi
+              border: "2px solid #ccc",
+              borderRadius: "8px",
               padding: "10px",
               margin: "5px 5px",
+              position: "relative",
             }}
           >
             <Box sx={{ alignSelf: "flex-start", mt: "-16px", ml: "-24px" }}>
@@ -92,8 +70,8 @@ const Dashboard = () => {
                 width: "100%",
               }}
             >
-        <ThemeToggleButton />
-          </Box>
+              <ThemeToggleButton />
+            </Box>
           </Panel>
         </PanelGroup>
       </Panel>
@@ -103,9 +81,8 @@ const Dashboard = () => {
       {/* Center Panel */}
       <Panel
         style={{
-          width: `${centerPanelWidth}%`,
-          border: "2px solid #ccc", // Obramowanie
-          borderRadius: "8px", // Zaokrąglone rogi
+          border: "2px solid #ccc",
+          borderRadius: "8px",
           margin: "10px 5px",
         }}
       >
@@ -121,13 +98,13 @@ const Dashboard = () => {
           flexDirection="column"
           width="100%"
           style={{
-            border: "2px solid #ccc", // Obramowanie
-            borderRadius: "8px", // Zaokrąglone rogi
+            border: "2px solid #ccc",
+            borderRadius: "8px",
             margin: "10px 5px",
           }}
         >
           <Box height="0%">
-            <NotesPanel />
+            <NotesPanel></NotesPanel>
           </Box>
 
           <Box flexGrow={1} overflow="auto">
@@ -136,7 +113,7 @@ const Dashboard = () => {
         </Box>
       </Panel>
     </PanelGroup>
-  );
-};
+  )
+}
 
 export default Dashboard
