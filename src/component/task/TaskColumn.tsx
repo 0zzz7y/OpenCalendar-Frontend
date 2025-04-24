faa50a1675c345f4110be5b9f8358601782fd873
ozzz7y <0zzz7y@gmail.com>
2025-04-24 05:14:21 +0200

    fixes

--- src/component/task/TaskColumn.tsx
+++ src/component/task/TaskColumn.tsx
@@ -9,32 +9,31 @@ interface Properties {
 
 const TaskColumn = ({ title, icon, children }: Properties) => {
   return (
-    <Paper
-      sx={{
-        minHeight: 500,
-        width: "100%", // This will allow TaskColumn to expand and take full available width
-        backgroundColor: "#fefefe",
-        borderRadius: 2,
-        p: 2,
-        boxShadow: 3,
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-      }}
-    >
-      <Box display="flex" alignItems="center" gap={1.5}>
-        <Box display="flex" alignItems="center" sx={{ mt: "-8px", color: "#000" }}>
-          {icon}
-        </Box>
-        <Box flex={1} display="flex" justifyContent="center">
-          <Typography variant="h6" fontWeight="bold" sx={{ color: "#000" }}>
-            {title}
-          </Typography>
-        </Box>
-      </Box>
+<Paper
+  sx={{
+    minHeight: 500,
+    width: "100%", // This will allow TaskColumn to expand and take full available width
+    backgroundColor: "#fefefe",
+    borderRadius: 2,
+    p: 2,
+    boxShadow: 3,
+    display: "flex",
+    flexDirection: "column",
+    gap: 2,
+  }}
+>
+  <Box display="flex" alignItems="center" justifyContent="center" gap={1.5}>
+    <Box display="flex" alignItems="center" sx={{ mt: "-8px", color: "#000" }}>
+      {icon}
+    </Box>
+    <Typography variant="h6" fontWeight="bold" sx={{ color: "#000" }}>
+      {title}
+    </Typography>
+  </Box>
 
-      {children}
-    </Paper>
+  {children}
+</Paper>
+
   )
 }
 
